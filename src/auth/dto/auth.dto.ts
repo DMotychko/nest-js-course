5ee5d45d16c648537c836b6b07ea4edcbe4120b9
Denis Motichko <motichkodenis10@gmail.com>
2025-02-01 11:47:05 +0100

    Add something

--- src/auth/dto/auth.dto.ts
+++
@@ -1,3 +0,0 @@
-export class CreateAuthDto {}
-
-export class AuthDto {}
--- src/common/entity/base.entity.ts
+++
@@ -1,9 +0,0 @@
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
-
-export class BaseEntity {
-  @CreateDateColumn()
-  created: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-}
